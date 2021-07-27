95a0823635f3577503b32d60219acaa34fa3a80c
fab7891 <fabrice_nokam@yahoo.fr>
2021-07-27 02:49:13 +0200

    Begin Service Fsseur

--- src/main/java/com/example/managershop/service/FournisseurService.java
+++ src/main/java/com/example/managershop/service/FournisseurService.java
@@ -1,6 +1,12 @@
 package com.example.managershop.service;
 
 
+import com.example.managershop.entities.Fournisseur;
+
+import java.util.List;
 
 public interface FournisseurService {
+
+    public List<Fournisseur> getAllFournisseur();
+    public Fournisseur findFsseurById(Long Idfsseur);
 }
--- src/main/java/com/example/managershop/service/Impl/FournisseurServiceImpl.java
+++ src/main/java/com/example/managershop/service/Impl/FournisseurServiceImpl.java
@@ -1,11 +1,28 @@
 package com.example.managershop.service.Impl;
 
+import com.example.managershop.dao.FounisseurRepository;
+import com.example.managershop.entities.Fournisseur;
 import com.example.managershop.service.FournisseurService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.List;
 
 @Service
 @Transactional
 public class FournisseurServiceImpl implements FournisseurService {
+
+    @Autowired
+    private FounisseurRepository founisseurRepository;
+    @Override
+    public List<Fournisseur> getAllFournisseur() {
+        return founisseurRepository.findAll();
+    }
+
+    @Override
+    public Fournisseur findFsseurById(Long idfsseur) {
+        if(!founisseurRepository.findById(idfsseur).isPresent())  throw new
+        return null;
+    }
 }
